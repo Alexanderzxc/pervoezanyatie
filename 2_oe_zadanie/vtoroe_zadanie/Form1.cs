077dff3049b4f4f440d5b7cc4f9d2c1df6614b30
Alexanderzxc <yaaa.22@uni-dubna.ru>
2023-09-15 21:25:35 +0300

    fisrt com

---
+++ 2_oe_zadanie/vtoroe_zadanie/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vtoroe_zadanie
+{
+    
+    public partial class Form1 : Form
+    {
+        stack stack1;
+        public Form1()
+        {
+            InitializeComponent();
+            stack1 = new stack(Convert.ToInt32(textBox1.Text));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stack1.Values(listBox1);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+            stack1.push();
+            stack1.Values(listBox1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            top_num.Text=Convert.ToString(stack1.pop());
+            stack1.Values(listBox1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            top_num.Text = Convert.ToString(stack1.peek());
+            stack1.Values(listBox1);
+        }
+    }
+}
---
+++ 2_oe_zadanie/vtoroe_zadanie/stack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace vtoroe_zadanie
+{
+    public class stack
+    {
+        private int currentindex;
+        private int[] mystack;
+
+        public int[] mass
+        {
+            get { return mystack; }
+        }
+        public stack(int razmer)
+        {
+            mystack = new int[razmer];
+            currentindex = 0;
+        }
+        
+        public void push()
+        {
+            if (mystack.Length > currentindex)
+            {
+                
+               Random rnd = new Random();
+               mystack[currentindex] = rnd.Next(1,150);
+               currentindex += 1;
+
+            }
+            else
+            {
+                MessageBox.Show("массив переполнен");
+            }
+        }
+        
+        public int pop()
+        {
+            var last = mystack[currentindex - 1];
+            mystack[currentindex - 1] = 0;
+            currentindex -= 1;
+            return last;
+        }
+
+        public int peek()
+        {
+            var last = mystack[currentindex-1];
+            return last;
+        }
+
+        public void Values(ListBox listBox)
+        {
+            listBox.Items.Clear();
+            foreach (var VARIABLE in mystack)
+            {
+                listBox.Items.Add(VARIABLE);
+            }
+        }
+        
+    }
+}
