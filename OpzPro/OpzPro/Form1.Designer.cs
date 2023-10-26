d2d976159bf575880988d5a887011deee44929f0
Alexanderzxc <upv.22@uni-dubna.ru>
2023-10-26 15:22:14 +0300

    RPN

---
+++ OpzPro/OpzPro/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace OpzPro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 40);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "12 2 + 3 *";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(36, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Result";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(210, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 50);
+            this.label1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+
+        #endregion
+    }
+}
---
+++ OpzPro/OpzPro/RPN.cs
@@ -0,0 +1,146 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+public class RPN
+{
+    private string input;
+    private string output;
+
+    public RPN(string inp)
+    {
+        input = inp;
+    }
+    public double Calculate()
+    {
+        GetExpression();
+        double result = Counting(); 
+        return result; 
+    }
+    
+    private void GetExpression()
+    { 
+        Stack<char> operStack = new Stack<char>();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            
+            if (IsDelimeter(input[i]))
+                 continue; 
+            
+            if (Char.IsDigit(input[i])) 
+            {
+                 while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                 {
+                     output += input[i];
+                     i++;
+    
+                     if (i == input.Length) break;
+                 }
+    
+                 output += " ";
+                 i--;
+            }
+            
+            if (IsOperator(input[i]))
+            {
+                if (input[i] == '(') 
+                    operStack.Push(input[i]); 
+                else if (input[i] == ')') 
+                {
+                    
+                    char s = operStack.Pop();
+    
+                    while (s != '(')
+                    {
+                        output += s.ToString() + ' ';
+                        s = operStack.Pop();
+                    }
+                }
+                else 
+                {
+                    if (operStack.Count > 0)
+                        if (GetPriority(input[i]) <= GetPriority(operStack.Peek()))
+                            output += Convert.ToString(operStack.Pop()) + " "; 
+    
+                    operStack.Push(char.Parse(input[i].ToString())); 
+    
+                }
+            }
+        }
+        
+        while (operStack.Count > 0)
+            output += operStack.Pop() + " ";
+        
+}
+    
+    private double Counting()
+    {
+        double result = 0; 
+        Stack<double> temp = new Stack<double>(); 
+
+        for (int i = 0; i < output.Length; i++) 
+        {
+            if (Char.IsDigit(output[i])) 
+            {
+                string a = string.Empty;
+
+                while (!IsDelimeter(output[i]) && !IsOperator(output[i]))
+                {
+                    a += output[i];
+                    i++;
+                    if (i == output.Length) break;
+                }
+                temp.Push(double.Parse(a));
+                i--;
+            }
+            else if (IsOperator(output[i]))
+            {
+                double a = temp.Pop(); 
+                double b = temp.Pop();
+
+                switch (output[i])
+                { 
+                    case '+': result = b + a; break;
+                    case '-': result = b - a; break;
+                    case '*': result = b * a; break;
+                    case '/': result = b / a; break;
+                    case '^': result = double.Parse(Math.Pow(double.Parse(b.ToString()), double.Parse(a.ToString())).ToString()); break;
+                }
+                temp.Push(result);
+            }
+        }
+        return temp.Peek();
+    }
+    
+    private int GetPriority(char s)
+    {
+        switch (s)
+        {
+            case '(': return 0;
+            case ')': return 1;
+            case '+': return 2;
+            case '-': return 3;
+            case '*': return 4;
+            case '/': return 4;
+            case '^': return 5;
+            default: return 6;
+        }
+    }
+    
+    private bool IsDelimeter(char c)
+    {
+        if ((" =".IndexOf(c) != -1))
+            return true;
+        return false;
+    }
+    
+    private bool IsOperator(char с)
+    {
+        if (("+-/*^()".IndexOf(с) != -1))
+            return true;
+        return false;
+    }
+    
+}
